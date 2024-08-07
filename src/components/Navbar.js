d5cf5694f8a703dd1b35e844c6cc0306365a959f
abdul <abdul.tanveer@gmail.com>
2024-08-07 12:19:03 +0530

    inline style for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,18 @@ import {NavLink} from 'react-router-dom'
 
 
 function Navbar() {
+
+  const navLinkStyles = ({isActive})=>{
+    return {
+      fontWeight: isActive ? 'bold':'normal',
+      textDecoration: isActive ? 'none':'underline'
+    }
+  }
+
   return (
     <nav>
-    <NavLink to='/'>Home</NavLink>
-    <NavLink to='/about'>About us</NavLink>
+    <NavLink to='/' style={navLinkStyles}>Home</NavLink>
+    <NavLink to='/about' style={navLinkStyles}>About us</NavLink>
 
     </nav>
   )
